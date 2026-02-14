f58d1dabcf1743bc88c221645e8466227fe4056a
Juan Coll <juancollsimoes@gmail.com>
2026-02-14 10:23:08 -0400

    Estructura inicial y librerias configuradas

---
+++ src/proyecto/pkg1/de/estructuras_graphstream/Nodo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.pkg1.de.estructuras_graphstream;
+
+/**
+ *
+ * @author andre
+ */
+public class Nodo {
+    
+}
