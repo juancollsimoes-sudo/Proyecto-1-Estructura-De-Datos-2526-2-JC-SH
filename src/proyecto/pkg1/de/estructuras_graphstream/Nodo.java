e7c1b455014a6cbd37405178c816bc25ede9e073
Juan Coll <juancollsimoes@gmail.com>
2026-02-20 09:15:12 -0400

    Modificacion de la clase Nodo: se implemento metodos para Dijkstra(distancia/predecesor) y grestion de grado para la identificacion de hubs

--- src/proyecto/pkg1/de/estructuras_graphstream/Nodo.java
+++ src/proyecto/pkg1/de/estructuras_graphstream/Nodo.java
@@ -19,7 +19,7 @@ public class Nodo {
     private double distanciaMinima = Double.MAX_VALUE; // Almacena la distancia acomulada desde el Nodo origen
     private boolean visitado = false; // detecta componentes conexos mediante BFS o DFS
     private Nodo predecesor = null; // referencia al nodo anterior en la ruta de menos resistencia, permite reconstruir la trayectoria de la ruta metabolica
-    
+    private int grado = 0; // sirve para identificar Hubs.
     public Nodo(Object elem){
         /**
          * Constructor de la clase Nodo.
@@ -29,4 +29,44 @@ public class Nodo {
         this.lista = new ListaAdyacencia();    
         this.pNext = null;
     }
+    /**
+     * Estos metodos son necesarios para el Algoritmo de Dijkstra.
+     * sirven para consultar y actualizar el peso del Nodo.
+     */
+    public double ObtenerDistanciaMinima(){
+        return distanciaMinima;
+    }
+    public void AgregarDistanciaMinima(double d){
+        this.distanciaMinima = d;
+    }
+    /**
+     * Estos metodos sirven para marcar por donde ya paso el Nodo.
+     * Es fundamental para Dijkstra y para DFS/DFS.
+     */
+    public boolean esVisitado(){
+        return visitado;
+    }
+    public void AgregarVisitado(boolean v){
+        this.visitado = v;
+    }
+    /**
+     * Estos metodos sirven para guardar el camino exacto para mostrar el nodo anterior.
+     */
+    public Nodo ObtenerPredecesor(){
+        return predecesor;
+    }
+    public void AgregarPredecesor(Nodo p){
+        this.predecesor = p;
+    }
+    /**
+     * Estos metodos permiten identificar "Hubs" o dianas terapeuticas primarias
+     * segun la cantidad de interacciones de la proteina.
+     */
+    public int ObtenerGrado(){
+        return grado;
+    }
+    public void IncrementoGrado(){
+        this.grado++;
+    }
+    
 }
