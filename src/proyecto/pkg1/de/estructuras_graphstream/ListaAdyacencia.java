1a9552b21d553abf3c62a95b139dce521e2a1b28
Shdzz <shd03072007@gmail.com>
2026-02-26 05:28:59 -0400

    git commit a las 5AM lmao
    le tengo miedo a las mujeres (si quieres que cambie algo escribeme) (obviamente no te voy a responder ahorita a las 7 pq voy a estar dormido)

--- src/proyecto/pkg1/de/estructuras_graphstream/ListaAdyacencia.java
+++ src/proyecto/pkg1/de/estructuras_graphstream/ListaAdyacencia.java
@@ -5,9 +5,46 @@
 package proyecto.pkg1.de.estructuras_graphstream;
 
 /**
- *
- * @author andre
+ * lista sencilla de adyacencia para un Nodo.
+ * 
+ * guarda los arcos (conexiones) como una lista enlazada
+ * usando la clase Arco.
  */
 public class ListaAdyacencia {
-    
+
+    // primer arco de la lista (cabeza)
+    private Arco primero;
+
+    /**
+     * crea una lista de adyacencia vacia.
+     */
+    public ListaAdyacencia() {
+        this.primero = null;
+    }
+
+    /**
+     * Agrega un nuevo arco al inicio de la lista.
+     * 
+     * @param destino nodo destino del arco
+     * @param peso    peso o costo de la conexion
+     */
+    public void Agregar(Nodo destino, double peso) {
+        Arco nuevo = new Arco(destino, peso);
+        nuevo.AgregarpNext(primero);
+        primero = nuevo;
+    }
+
+    /**
+     * @return el primer arco de la lista (puede ser null si esta vacia)
+     */
+    public Arco ObtenerPrimero() {
+        return primero;
+    }
+
+    /**
+     * @return true si la lista no tiene arcos
+     */
+    public boolean EstaVacia() {
+        return primero == null;
+    }
 }
