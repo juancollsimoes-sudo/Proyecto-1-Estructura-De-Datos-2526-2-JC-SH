7c2a753f2849f393fa70ae5bca49f87dcf1f4a77
Juan Coll <juancollsimoes@gmail.com>
2026-02-27 08:50:25 -0400

    Creacion de la clase grafo: inicializando sus atributos, y crwando los metodos AgregarNodo y AgregarArco.

--- src/proyecto/pkg1/de/estructuras_graphstream/Grafo.java
+++ src/proyecto/pkg1/de/estructuras_graphstream/Grafo.java
@@ -5,9 +5,52 @@
 package proyecto.pkg1.de.estructuras_graphstream;
 
 /**
- *
- * @author andre
+ * 
+ * @author Juan Coll
  */
 public class Grafo {
+    private Nodo pFirst;
+    private int iN;
+    private Nodo HubPrincipal;
+    
+    /**
+     * Constructor de la clase grafo
+     * Inicializa un grafo Vacio
+     */
+    public Grafo() {
+        this.pFirst = null;
+        this.iN = 0;
+        this.HubPrincipal = null;
+    }
+    /**
+     * Agrega un nuevo nodo/proteina al grafo.
+     * @param dato es el objeto que representa a la proteina
+     * @return el nodo pNew
+     */
+    public Nodo AgregarNodo(Object dato){
+        Nodo pNew = new Nodo(dato);
+        pNew.pNext = pFirst;
+        pFirst = pNew;
+        iN++;
+        return pNew;
+    }
+    
+    /**
+     * Agrega un Arco entre dos Proteinas/Nodos
+     * @param pOrigen es el Nodo de origen de la conexion.
+     * @param pDestino es el Nodo destino de la conexion.
+     * @param peso es la resistencia de la conexion.
+     */
+    public void AgregarArco(Nodo pOrigen, Nodo pDestino, double peso){
+        if (pOrigen != null && pDestino != null){
+            pOrigen.lista.Agregar(pDestino, peso);
+            pOrigen.IncrementoGrado();
+            // actualiza el Hub si y solo si este nodo tiene mas conexiones.
+            if (HubPrincipal == null || pOrigen.ObtenerGrado() > HubPrincipal.ObtenerGrado()){
+                HubPrincipal = pOrigen;
+            }
+        }
+    }
+    
     
 }
